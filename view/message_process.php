d19c109745f23df89271368f2720e0c4d1d08e9e
DESKTOP-OMQTFCT\vefetch <asudhakar@live.in>
2016-09-15 15:35:51 +0530

    testing

--- view/message_process.php
+++ view/message_process.php
@@ -1,7 +1,7 @@
 <?php 
 include('../controllers/function.php');
-$names = $_POST['name'];
-$path = $_POST['path'];
+$names = $_GET['name'];
+$path = $_GET['path'];
 $name_and_number = base64_encode(serialize($names));
 print_r($names);
 
